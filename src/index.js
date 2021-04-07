8cc084fb2ea0ca0391fc3edc82c31e5ef3937ca7
Jakub Vala <jamm3e3333@gmail.com>
2021-04-08 00:43:12 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const hbs = require('hbs');
 require('./db/mongoDB.js');
-const path = require('path');
 const routerUser = require('./routers/users.js');
 const routerTask = require('./routers/tasks.js');
 
-const publicPath = path.join(__dirname, '../public');
-const templatePath = path.join(__dirname, './templates/views');
 const app = express();
 const port = process.env.PORT;
 
-app.set('view engine','hbs');
-app.set('views', templatePath);
 
-app.use(express.static(publicPath));
 app.use(express.json());
 app.use(routerUser);
 app.use(routerTask);
