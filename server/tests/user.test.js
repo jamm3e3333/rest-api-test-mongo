7ae83dcfdda58f701b1c4db30818e069825aba2c
Jam3e3333 <jamm3e3333@gmail.com>
2021-09-07 19:14:19 -0400

    trying tests

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -1,11 +1,38 @@
 const request = require('supertest');
 const app = require('../src/app.js');
+const mongoose = require('mongoose');
+const User = require('../src/models/users');
+const jwt = require('jsonwebtoken');
+
+const userId = new mongoose.Types.ObjectId();
+
+const token = jwt.sign({_id: userId.toString()}, process.env.JWT_SECRET);
+
+const newUser = {
+    _id: userId,
+    email: 'mod88@seznam.cz',
+    password: process.env.TEST_PASS,
+    nick: 'mod88',
+    age: 150,
+    tokens: {
+        _id: userId.toString(),
+        token
+    }
+}
+
+beforeEach(async () => {
+    await new User(newUser).save();
+});
+
+
+afterEach(async () => {
+    await User.deleteOne({_id: userId});
+})
 
 test('Should signup a new user', async() => {
-    await (await request(app).post('/users/create')).send({
-        email: 'jakub.valaa@seznam.cz',
-        nick: 'jakoub009',
-        password: 'jakoub323',
-        age: 32
-    }).expect(201)
+    const response = await request(app)
+                            .get('http://localhost:3010/users/me')
+                            .set('Authorization',`Bearer ${token}`)
+                            .expect(200)
+
 })
